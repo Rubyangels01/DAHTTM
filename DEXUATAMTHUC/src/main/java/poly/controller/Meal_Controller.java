6c989cd7c4cad2713c99efc148cbb054872339e9
Rubyangels01 <126937477+Rubyangels01@users.noreply.github.com>
2023-10-15 22:52:17 +0700

    Update Meal_Controller.java

--- DEXUATAMTHUC/src/main/java/poly/controller/Meal_Controller.java
+++ DEXUATAMTHUC/src/main/java/poly/controller/Meal_Controller.java
@@ -1,13 +1,28 @@
 package poly.controller;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import poly.DAO.MEAL_DAO;
+
+
 
 @Controller
 public class Meal_Controller {
 	@RequestMapping(value = "users/Main", method = RequestMethod.GET)
 	public String Home() {
 		return "users/Main";
+	}
+	@RequestMapping(value = "users/mainfood", method = RequestMethod.GET)
+	public String MainHomePage(Model model) throws SQLException {
+		MEAL_DAO meal_dao = new MEAL_DAO();
+	    model.addAttribute("MainFood", meal_dao.getTypeFood(1));
+	    model.addAttribute("DessertFood",meal_dao.getTypeFood(2));
+	    model.addAttribute("BreakFast",meal_dao.getTypeFood(3));
+	    return "users/Category";
 	}
 }
