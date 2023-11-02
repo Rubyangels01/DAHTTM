51adb1c7ab4f9ce682065360e2648700de868190
Rubyangels01 <126937477+Rubyangels01@users.noreply.github.com>
2023-11-02 21:36:02 +0700

    Update Meal_Controller.java

--- DEXUATAMTHUC/src/main/java/poly/controller/Meal_Controller.java
+++ DEXUATAMTHUC/src/main/java/poly/controller/Meal_Controller.java
@@ -1,33 +1,45 @@
 package poly.controller;
 
+import java.net.http.HttpRequest;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import poly.DAO.MEAL_DAO;
-import poly.bean.MEAL;
+import poly.DAO.*;
+import poly.bean.*;
+
+
 
 
 
 @Controller
 public class Meal_Controller {
-	
+	 int TIMKIEM,LUU = 0;
+	  int YEUTHICH,XEM = 1;
 	@RequestMapping(value = "users/Main", method = RequestMethod.GET)
-	public String Home(Model model) throws SQLException {
+	public String HOME(Model model) throws SQLException {
 		MEAL_DAO meal_dao = new MEAL_DAO();
-	    model.addAttribute("famousfood",meal_dao.getFamousFood());
+	    model.addAttribute("famousfood",meal_dao.GET_FAMOUSFOOD());
 		return "users/Main";
 	}
 	@RequestMapping(value = "users/mainfood", method = RequestMethod.GET)
-	public String Catogory(Model model) throws SQLException {
+	public String CATOGORY(Model model) throws SQLException {
 		MEAL_DAO meal_dao = new MEAL_DAO();
 	    model.addAttribute("MainFood", meal_dao.getTypeFood(1));
 	    model.addAttribute("DessertFood",meal_dao.getTypeFood(2));
@@ -37,11 +49,12 @@ public class Meal_Controller {
 	@RequestMapping(value = "users/famousfood", method = RequestMethod.GET)
 	public String List_Famous(Model model) throws SQLException {
 		MEAL_DAO meal_dao = new MEAL_DAO();
-	    model.addAttribute("famousfood",meal_dao.getFamousFood());
+	    model.addAttribute("famousfood",meal_dao.GET_FAMOUSFOOD());
+	    
 	    return "users/Famous_List";
 	}
 	@RequestMapping(value = "users/suggest", method = RequestMethod.GET)
-	public String listSuggest(Model model, HttpSession session) throws SQLException {
+	public String LIST_SUGGEST(Model model, HttpSession session) throws SQLException {
 	    MEAL_DAO mealDao = new MEAL_DAO();
 	    
 	    // Kiểm tra xem session có tồn tại giá trị "maUser" hay không
@@ -60,28 +73,231 @@ public class Meal_Controller {
 	}
 	
 	@RequestMapping(value = "users/Infor_Meal{id}", method = RequestMethod.GET)
-	public String productDetail(@PathVariable("id") String melaId, ModelMap model, HttpServletRequest request)
+	public String DETAIL_MEAL(@PathVariable("id") String melaId, ModelMap model, HttpServletRequest request,HttpSession session)
 			throws SQLException {
 		// xử lý mã sản phẩm và chuẩn bị dữ liệu
-
 		MEAL_DAO mealDao = new MEAL_DAO();
+		Integer maUser = (Integer) session.getAttribute("maUser");
 		for (MEAL meal : mealDao.getAllMeal()) {
 			if (meal.getMaMon() == Integer.parseInt(melaId)) {
 				
+				request.setAttribute("maMon", meal.getMaMon());
 					request.setAttribute("imgUrl", meal.getHinhAnh());
 					request.setAttribute("tenMon", meal.getTenMon());
+					
 					 model.addAttribute("NguyenLieu",mealDao.GET_NGUYENLIEU(meal.getMaMon()));
 					 model.addAttribute("BuocLam",mealDao.GET_STEP(meal.getMaMon()));
+					 mealDao.VIEWORFIND(maUser, meal.getMaMon(),XEM);
 					return "users/DeTail_Meal";
-
 				}
-
 			} 
 		return "users/DeTail_Meal";
-
-
 		// trả về view hiển thị chi tiết sản phẩm
 	}
+	@RequestMapping("users/saveandlike")
+	public ResponseEntity<String> saveAndLike(HttpSession session, @RequestParam("action") String action, @RequestParam("idmonan") int idmonan) throws SQLException {
+	    MEAL_DAO meal_DAO = new MEAL_DAO();
+	    Integer maUser = (Integer) session.getAttribute("maUser");
+//	    request.setAttribute("message", idmonan);
+	    boolean result_save = true;
+	    @SuppressWarnings("unused")
+		boolean result_like= true;
+	    if (action.equals("luu")) {
+	    	 if(meal_DAO.CHECK_LIKEORSAVE(maUser, idmonan, LUU) == 0)
+	    	 {
+	    		 meal_DAO.SAVEORLIKE(maUser, idmonan, LUU);
+	    	 }
+	    	 else {
+	    		 result_save = false;
+			}
+	       
+	    } else if (action.equals("yeuThich")) {
+	    	 if(meal_DAO.CHECK_LIKEORSAVE(maUser, idmonan,1) == 0)
+	    	 {
+	    		 meal_DAO.SAVEORLIKE(maUser, idmonan, 1);
+	    	 }
+	    	 else {
+	    		 result_like = false;
+			}
+	    }
+	    if (result_save) {
+	    	return new ResponseEntity<>("Món đã được lưu!", HttpStatus.OK);         
+        } else {
+        	return new ResponseEntity<>("Lưu thành công!", HttpStatus.OK);           
+        }
+	   
+
+	    
+	}
+	
+	@RequestMapping(value = "users/createmeal", method = RequestMethod.GET)
+	public String CREATE_MEAL(Model model) throws SQLException {
+		MEAL_DAO meal_DAO = new MEAL_DAO();
+		model.addAttribute("NguyenLieu",meal_DAO.GET_INGREDIENT());
+		model.addAttribute("loaiMon",meal_DAO.GET_TYPEMEAL());
+		return "users/Create_Meal";
+	}
+	
+	
+	@RequestMapping("users/checksearch")
+	public String CHECK_SEARCH(HttpServletRequest request, Model model) throws SQLException {
+
+	    MEAL_DAO meal_DAO = new MEAL_DAO();
+	    ArrayList<MEAL> Meal_Find = new ArrayList<MEAL>();
+	    String input_search = request.getParameter("search");
+	    String[] ingredientArray = input_search.split(",\\s*");
+	    List<String> ingredientList = Arrays.asList(ingredientArray);
+
+	    for (MEAL meal : meal_DAO.getAllMeal()) {
+	        boolean containsAll = true;
+
+	        for(String nguyenlieu : ingredientList) {
+	            boolean containsIngredient = false;
+	            for (NGUYENLIEU_MONAN nglieumon : meal_DAO.GET_NGUYENLIEU(meal.getMaMon())) {
+	                if (nglieumon.getTenNguyenLieu().equalsIgnoreCase(nguyenlieu.trim())) {
+	                    containsIngredient = true;
+	                    break;
+	                }
+	            }
+
+	            if (!containsIngredient) {
+	                containsAll = false;
+	                break;
+	            }
+	        }
+
+	        if (containsAll) {
+	            Meal_Find.add(meal);
+	        }
+	    }
+	    model.addAttribute("message", meal_DAO.taoMaCongThuc());
+	    // Thực hiện các thao tác cần thiết với Meal_Find, ví dụ như gửi về frontend
+	    model.addAttribute("foundMeals", Meal_Find);
+
+	    return "users/List_Find";
+	}
+	@RequestMapping("users/checkmeal")
+	public String CHECK_MEAL(HttpServletRequest request, Model model,HttpSession session,@ModelAttribute("ListNguyenLieu") ListNguyenLieu formModel) throws SQLException {
+
+	    MEAL_DAO meal_DAO = new MEAL_DAO();
+	    ArrayList<MEAL> Meal_Find = new ArrayList<MEAL>();
+	    Integer maUser = (Integer) session.getAttribute("maUser");
+	    // Lấy thông tin từ request
+	    String name = request.getParameter("tenmon");
+	    String descri = request.getParameter("mota");
+	    String level = request.getParameter("dokho");
+	    
+	    // Chuyển đổi dữ liệu từ String sang int
+	    int hour = Integer.parseInt(request.getParameter("hour"));
+	    int minute = Integer.parseInt(request.getParameter("minute"));
+
+	    int typeMeal = Integer.parseInt(request.getParameter("loaimon"));
+	   
+
+	    
+
+	    // Gọi hàm CREATE_RECIPE với các tham số đã lấy được
+	   meal_DAO.CREATE_RECIPE(hour, minute, level);
+	   meal_DAO.CREATE_MEAL(name, descri, typeMeal, maUser, meal_DAO.maCongThuc);
+	   for(NguyenLieu_SoLuong nglieumon : formModel.getCT_NguyenLieuList())
+	   {
+		   meal_DAO.CREATE_INGREDIENT(nglieumon.getIdNguyenLieu(),nglieumon.getSoLuong() );
+	   }
+	  for(int i = 0; i < formModel.getCT_BuocLam().size();i++)
+	  {
+		  meal_DAO.CREATE_STEP(i+1,formModel.getCT_BuocLam().get(i).getMoTa());
+	  }	  
+	  
+//	  request.setAttribute("imgUrl", meal_DAO.getMealById(meal_DAO.maCongThuc));
+		request.setAttribute("tenMon", name);
+		
+		 model.addAttribute("NguyenLieu",meal_DAO.GET_INGREDIENT_FROM_RECIPE(meal_DAO.maCongThuc));
+		 model.addAttribute("BuocLam",meal_DAO.GET_STEP_FROM_IDRECIPE(meal_DAO.maCongThuc));
+	    return "users/Meal_Created";
+	}
+	
+	
+
+
+
+	@RequestMapping(value = "users/findmeal", method = RequestMethod.GET)
+	public String FIND_MEAL(Model model) throws SQLException {
+		
+		
+		return "users/Find_Meal";
+	}
+	@RequestMapping("users/searchmeal")
+	public String CHECK_FIND_MEAL(HttpServletRequest request, Model model,HttpSession session) throws SQLException {
+		
+	    MEAL_DAO meal_DAO = new MEAL_DAO();	
+	    String input_search = request.getParameter("search");
+	    Integer maUser = (Integer) session.getAttribute("maUser");
+	    if(input_search == null || input_search.isEmpty())
+	    {
+	    	request.setAttribute("message", "Vui lòng nhập từ khoá mà bạn muốn tìm kiếm!");
+	    }
+	    else {
+	    	 ArrayList<MEAL> SORTED_LIST = meal_DAO.filterFoodList(meal_DAO.getAllMeal(),input_search);
+	    	 if(SORTED_LIST == null || SORTED_LIST.isEmpty())
+	    	 {
+	    		 request.setAttribute("message", "Danh sách tìm kiếm rỗng");
+	    	 }
+	    	 meal_DAO.sortFoodList(SORTED_LIST,input_search);
+	 	    request.setAttribute("input_search", input_search);
+	 	    model.addAttribute("FindMeal", SORTED_LIST);
+	 	   for(MEAL meal : SORTED_LIST)
+	 	   {
+	 		   meal_DAO.VIEWORFIND(maUser, meal.getMaMon(), TIMKIEM);
+	 	   }
+		}
+	    
+	    
+	   
+	   
+	   
+	    return "users/Find_Meal";
+	}
+
+	@RequestMapping(value = "users/mealuser", method = RequestMethod.GET)
+	public String MEAL_USER(Model model) throws SQLException {	
+		MEAL_DAO meal_DAO = new MEAL_DAO();
+		
+		model.addAttribute("List_Saved",meal_DAO.getAllMeal());
+		return "users/MonAn_User";
+	}
+	@RequestMapping(value = "users/EditMeal{id}", method = RequestMethod.GET)
+	public String EDIT(@PathVariable("id") String melaId, ModelMap model, HttpServletRequest request,HttpSession session)
+			throws SQLException {
+		
+		
+		return "users/";
+	}
+	@RequestMapping(value = "users/DeleteMeal{id}", method = RequestMethod.GET)
+	public String DELETE(@PathVariable("id") int melaId, ModelMap model, HttpServletRequest request,HttpSession session)
+			throws SQLException {
+		MEAL_DAO meal_DAO = new MEAL_DAO();
+		meal_DAO.DELETE_MEAL(melaId);	
+		return "users/MonAn_User";
+	}
+//	@RequestMapping(value = "users/test1", method = RequestMethod.GET)
+//	public String CREATE_MEAL1(Model model) throws SQLException {
+//		MEAL_DAO meal_DAO = new MEAL_DAO();
+//		model.addAttribute("NguyenLieu", meal_DAO.GET_INGREDIENT());
+//		model.addAttribute("greeting", "Xin chào");
+//		return "users/test";
+//	}
+//	@RequestMapping("users/test")
+//	public String processForm(@ModelAttribute("ListNguyenLieu") ListNguyenLieu formModel, HttpServletRequest request) {
+//	    // Xử lý dữ liệu
+//	    if (formModel.getCT_NguyenLieuList() != null) {
+//	        request.setAttribute("message", formModel.getCT_NguyenLieuList().get(1).getIdNguyenLieu());
+//	        request.setAttribute("message1", formModel.getCT_BuocLam().get(1).getBuocLam());
+//	    } else {
+//	        request.setAttribute("message", "Danh sách rỗng hoặc null");
+//	    }
+//
+//	    return "users/test2";
+//	}
 //	@RequestMapping(value = "users/suggest", method = RequestMethod.GET)
 //	public String suggest() {
 //		return "users/Suggest";
